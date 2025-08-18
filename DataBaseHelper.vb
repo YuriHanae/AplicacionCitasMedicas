14fa14b1689426bbdd4759df968b9a7e3e56e413
matthew bermudez <bequimatt.2005@gmail.com>
2025-08-17 22:13:15 -0600

    Añadir páginas de inicio de sesión y registro
    Se han creado las páginas ASP.NET `Login.aspx` y `Registrar.aspx`, junto con sus archivos de código y diseñador. Se ha implementado la clase `DataBaseHelper` para verificar credenciales de pacientes. Además, se ha actualizado `Web.config` con una cadena de conexión a la base de datos y se han añadido validaciones en el formulario de registro. Los archivos de proyecto han sido actualizados para incluir estas nuevas funcionalidades.

---
+++ DataBaseHelper.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Data.SqlClient
+
+Public Class DataBaseHelper
+    Private ReadOnly connectionString As String = ConfigurationManager.ConnectionStrings("Login").ConnectionString
+
+    'Public Function GetPacientes() As List(Of Paciente)
+    '    Dim pacientes As New List(Of Paciente)()
+    '    Using connection As New SqlConnection(connectionString)
+    '        connection.Open()
+    '        Dim command As New SqlCommand("SELECT * FROM Pacientes", connection)
+    '        Using reader As SqlDataReader = command.ExecuteReader()
+    '            While reader.Read()
+    '                Dim paciente As New Paciente()
+    '                paciente.IdPaciente = reader("PacienteId")
+    '                paciente.Nombre = reader("Nombre")
+    '                paciente.Email = reader("Email")
+    '                paciente.Telefono = reader("Telefono")
+    '                paciente.Contrasena = reader("Contrasena")
+    '                pacientes.Add(paciente)
+    '            End While
+    '        End Using
+    '    End Using
+    '    Return clientes
+    'End Function
+
+    'Public Sub InsertarCliente(cliente As Cliente)
+    '    Using connection As New SqlConnection(connectionString)
+    '        connection.Open()
+    '        Dim command As New SqlCommand("INSERT INTO Clientes (Nombre, Email, Telefono) VALUES (@Nombre, @Email, @Telefono)", connection)
+    '        command.Parameters.AddWithValue("@Nombre", cliente.Nombre)
+    '        command.Parameters.AddWithValue("@Email", cliente.Email)
+    '        command.Parameters.AddWithValue("@Telefono", cliente.Telefono)
+    '        command.ExecuteNonQuery()
+    '    End Using
+    'End Sub
+
+    'Public Sub ActualizarCliente(cliente As Cliente)
+    '    Using connection As New SqlConnection(connectionString)
+    '        connection.Open()
+    '        Dim command As New SqlCommand("UPDATE Clientes SET Nombre = @Nombre, Email = @Email, Telefono = @Telefono WHERE IdCliente = @IdCliente", connection)
+    '        command.Parameters.AddWithValue("@IdCliente", cliente.IdCliente)
+    '        command.Parameters.AddWithValue("@Nombre", cliente.Nombre)
+    '        command.Parameters.AddWithValue("@Email", cliente.Email)
+    '        command.Parameters.AddWithValue("@Telefono", cliente.Telefono)
+    '        command.ExecuteNonQuery()
+    '    End Using
+    'End Sub
+
+    'Public Sub EliminarCliente(idCliente As Integer)
+    '    Using connection As New SqlConnection(connectionString)
+    '        connection.Open()
+    '        Dim command As New SqlCommand("DELETE FROM Clientes WHERE IdCliente = @IdCliente", connection)
+    '        command.Parameters.AddWithValue("@IdCliente", idCliente)
+    '        command.ExecuteNonQuery()
+    '    End Using
+    'End Sub
+
+    Public Function VerificarCredenciales(cliente As Paciente) As Boolean
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+            Dim command As New SqlCommand("SELECT  Usuario,Contraseña  FROM Usuarios WHERE Usuario = @Usuario AND Contraseña = @Contraseña", connection)
+            command.Parameters.AddWithValue("@Usuario", cliente.Email)
+            command.Parameters.AddWithValue("@Contraseña", cliente.Contraseña)
+            Dim reader As SqlDataReader = command.ExecuteReader()
+            Return reader.HasRows
+        End Using
+    End Function
+End Class
---
+++ Login.aspx.vb
@@ -0,0 +1,26 @@
+﻿Public Class Login
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnLogin_Click(sender As Object, e As EventArgs)
+        If VerificarCredenciales() Then
+            Response.Redirect("Default.aspx")
+
+            lblError.Text = "Credenciales inválidas"
+            lblError.Visible = True
+        End If
+    End Sub
+
+    Protected Function VerificarCredenciales() As Boolean
+        Dim paciente As New Paciente() With {
+            .Email = txtEmail.Text,
+            .Contraseña = txtPass.Text
+        }
+
+        Dim Helper As New DataBaseHelper()
+        Return Helper.VerificarCredenciales(paciente)
+    End Function
+End Class
