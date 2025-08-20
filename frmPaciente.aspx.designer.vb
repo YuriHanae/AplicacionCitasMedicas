ba8e5e9bd235eb6679b15f8b2a0c60551f679541
matthew bermudez <bequimatt.2005@gmail.com>
2025-08-20 17:38:53 -0600

    Renombrar control lblMensj a lblMens y añadir Option Explicit
    Se ha cambiado el nombre del control `lblMensj` a `lblMens` en el archivo `frmPaciente.aspx.designer.vb`, lo que puede afectar su uso en el código subyacente y en la interfaz de usuario. También se ha añadido la directiva `Option Explicit On` para mejorar la claridad y seguridad del código al obligar a declarar todas las variables antes de usarlas.

--- frmPaciente.aspx.designer.vb
+++ frmPaciente.aspx.designer.vb
@@ -14,11 +14,11 @@ Option Explicit On
 Partial Public Class frmPaciente
 
     '''<summary>
-    '''Control lblMensj.
+    '''Control lblMens.
     '''</summary>
     '''<remarks>
     '''Campo generado automáticamente.
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
-    Protected WithEvents lblMensj As Global.System.Web.UI.WebControls.Label
+    Protected WithEvents lblMens As Global.System.Web.UI.WebControls.Label
 End Class
